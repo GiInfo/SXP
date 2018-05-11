7e31376ec1ee73a95cafe6a6bcf63f1bb87671f4
fredze <zembo.alfred@gmail.com>
2018-05-11 11:15:29 +0200

    fix some javascript escape error

--- src/main/js/main-electron.js
+++ src/main/js/main-electron.js
@@ -21,7 +21,7 @@ function createWindow() {
     //win.openDevTools();
 
     // Emitted when the window is closed.
-    win.on('closed', () = > {
+    win.on('closed', () => {
         // Dereference the window object, usually you would store windows
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
@@ -36,7 +36,7 @@ function createWindow() {
 app.on('ready', createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', () = > {
+app.on('window-all-closed', () => {
     // On OS X it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
     if(process.platform !== 'darwin'
@@ -47,7 +47,7 @@ app.on('window-all-closed', () = > {
 })
 ;
 
-app.on('activate', () = > {
+app.on('activate', () => {
     // On OS X it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if(win === null
