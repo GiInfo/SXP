46ab8d10001708f25e16fca404374a375b732f4b
fredze <zembo.alfred@gmail.com>
2018-05-15 23:38:47 +0200

    add controller just for simple message chat

--- src/main/java/controller/Messages.java
+++ src/main/java/controller/Messages.java
@@ -48,6 +48,200 @@ import rest.api.ServletPath;
 @Path("/")
 public class Messages {
     private final static Logger log = LogManager.getLogger(Message.class);
+    
+    @GET
+	@Path("/private")
+	@Produces(MediaType.APPLICATION_JSON)
+	public ChunkedOutput<String> getPrivate(@HeaderParam(Authentifier.PARAM_NAME) final String token) {
+
+		Authentifier auth = Application.getInstance().getAuth();
+		UserSyncManager users = SyncManagerFactory.createUserSyncManager();
+		final User currentUser = users.getUser(auth.getLogin(token), auth.getPassword(token));
+		users.close();
+		
+		final ChunkedOutput<String> output = new ChunkedOutput<String>(String.class);
+		
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				
+				JsonTools<Collection<Message>> json = new JsonTools<>(new TypeReference<Collection<Message>>(){});
+				
+				Manager<Message> em = ManagerFactory.createNetworkResilianceMessageManager(Application.getInstance().getPeer(), token, currentUser,null);
+				
+				final Hashtable<String, Message> hashtableMessage = new Hashtable<>(); 
+				
+				em.findAllByAttribute("receiverId", currentUser.getId(), new ManagerListener<Message>() {
+					@Override
+					public void notify(Collection<Message> results) {
+						
+						for (Iterator iterator = results.iterator(); iterator.hasNext();) {
+							Message message = (Message) iterator.next();
+							if(hashtableMessage.get(message.getId())==null){
+								hashtableMessage.put(message.getId(), message);
+							}
+						}
+					}
+				});
+				
+				em.findAllByAttribute("senderId", currentUser.getId(), new ManagerListener<Message>() {
+					@Override
+					public void notify(Collection<Message> results) {
+						for (Iterator iterator = results.iterator(); iterator.hasNext();) {
+							Message message = (Message) iterator.next();
+							if(hashtableMessage.get(message.getId())==null){
+								hashtableMessage.put(message.getId(), message);
+							}
+						}
+					}
+				});
+				
+				try {
+					
+					Thread.sleep(3000);
+					
+					output.write(json.toJson(hashtableMessage.values()));
+					
+				} catch (InterruptedException e) {
+					LoggerUtilities.logStackTrace(e);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				finally {
+					try {
+						output.write("[]");
+						output.close();
+					} catch (IOException e) {
+						LoggerUtilities.logStackTrace(e);
+					}
+				}
+				em.close();
+			}
+		}).start();
+		
+		return output;
+	}
+    
+    @POST
+	@Path("/private")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public ChunkedOutput<String> addPrivate(final Message message, @HeaderParam(Authentifier.PARAM_NAME) final String token) {
+		
+    	Authentifier auth = Application.getInstance().getAuth();
+        UserSyncManager users = SyncManagerFactory.createUserSyncManager();
+        final User sender = users.getUser(auth.getLogin(token), auth.getPassword(token));
+
+        final ChunkedOutput<String> output = new ChunkedOutput<String>(String.class);
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+
+                final ArrayList<User> usernames = new ArrayList<>();
+
+                Manager<User> usem = ManagerFactory.createNetworkResilianceUserManager(Application.getInstance().getPeer(), token);
+				
+				usem.findAllByAttribute("nick",message.getReceiverName(), new ManagerListener<User>() {
+					@Override
+					public void notify(Collection<User> results) {
+						
+						for (Iterator iterator = results.iterator(); iterator.hasNext();) {
+							User user = (User) iterator.next();
+							usernames.add(user);
+							break;
+						}
+					}
+				});
+                try {
+                    Thread.sleep(3000);
+                    usem.close();
+                } catch (InterruptedException e) {
+                    LoggerUtilities.logStackTrace(e);
+                }
+
+
+                /**
+                 * Because we have to actually send a message to each member of a chat/forum we copy the message
+                 * And to keep track of each forum a new ID was put in place. the Chat ID.
+                 * This distinction could be use in the future to implement a read receipt like in fb/whatsapp
+                 */
+                
+               
+                if (usernames != null) {
+    				User user = usernames.size()>0? usernames.get(0):null;
+                    Date sendDate = new Date();
+                    String uuid ;
+                    if(message.getChatID()==null){
+                        uuid = UUID.randomUUID().toString();
+                    }else{
+                        uuid = message.getChatID();
+                    }
+
+                Message aMessage = new Message();
+                aMessage.setSendingDate(sendDate);
+                aMessage.setSender(sender.getId(), sender.getNick());
+                aMessage.setPbkey(sender.getKey().getPublicKey());
+                aMessage.setReceivers(message.getReceivers());
+                aMessage.setMessageContent(message.getMessageContent());
+                aMessage.setReceiver(user.getId(),user.getNick());
+                aMessage.setContractTitle(null);
+                aMessage.setContractID(null);
+                aMessage.setChatID(uuid);
+                aMessage.setReceiversNicks(message.getReceiversNicks());
+                aMessage.setContractID(message.getContractID());
+                Manager<Message> em = ManagerFactory.createNetworkResilianceMessageManager(Application.getInstance().getPeer(), token, user, sender);
+                boolean pushDbOk = em.begin();
+                pushDbOk &= em.persist(aMessage);
+                pushDbOk &= em.end();
+                pushDbOk &= em.close();
+
+
+                if (!pushDbOk) {
+                    log.warn("Message might not have been sent.");
+                    try {
+                        output.write("{\"error\": \"Message might not have been sent.\"}");
+                    } catch (IOException e) {
+                        LoggerUtilities.logStackTrace(e);
+                    }
+                }
+
+                em.close();
+
+                JsonTools<Message> json = new JsonTools<>(new TypeReference<Message>() {
+                });
+                try {
+                    output.write(json.toJson(message));
+                } catch (IOException e) {
+                    LoggerUtilities.logStackTrace(e);
+                }
+                        
+
+                    
+
+                } else {
+
+                    try {
+                        output.write("{\"error\": \"No receiver specified.\"}");
+                    } catch (IOException e) {
+                        LoggerUtilities.logStackTrace(e);
+                    }
+
+                }
+
+                try {
+                    output.write("[]");
+                    output.close();
+                } catch (IOException e) {
+                    LoggerUtilities.logStackTrace(e);
+                }
+
+            }
+        }).start();
+		
+		return output;
+	}
 
     @POST
     @Path("/")
